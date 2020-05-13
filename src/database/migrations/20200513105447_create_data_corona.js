b6b1f850786e2b71afce9b0c5d939057268f21a9
macedo-developer <macedorenata.developer@gmail.com>
2020-05-13 11:11:29 -0300

    First Send

---
+++ src/database/migrations/20200513105447_create_data_corona.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("data_corona", (table) => {
+    table.increments("id");
+    table.date("date").notNullable();
+    table.integer("newConfirmed").notNullable();
+    table.integer("newDeaths").notNullable();
+    table.integer("newRecovered").notNullable();
+    table.integer("totalConfirmed").notNullable();
+    table.integer("totalDeaths").notNullable();
+    table.integer("totalRecovered").notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("data_corona");
+};
